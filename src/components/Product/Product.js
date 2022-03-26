9bed1f6ec6b47e65d5cb70793978d2093d6b48b3
Md Warej Ali <waraj.com@gmail.com>
2022-03-26 22:48:36 +0600

    add id

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,7 @@ import { BsFillCartPlusFill } from 'react-icons/bs';
 import './Product.css'
 
 const Product = (props) => {
-    const {name, price, img} = props.product
+    const {name, price, img, id} = props.product
     return (
         
             <div className="col">
@@ -11,6 +11,7 @@ const Product = (props) => {
                     <img src={img} alt="" className="card-img-top"/>
                     <div className ="card-body">
                         <h5 className="card-title">{name}</h5>
+                        <h6>ID: {id}</h6>
                         <h5>Price: ${price}</h5>
                     </div>
                     <div className="card-footer btn-add-cart">
